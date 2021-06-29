2f312c8f3ace6d193d068a7d7903bae36fd69dc0
Maksim <max.freedman.main@gmail.com>
2021-06-29 23:07:38 +0300

    Added CustomUserDetailsService class

---
+++ src/main/java/opera/app/spring/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package opera.app.spring.service;
+
+import opera.app.spring.model.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user;
+        try {
+            user = userService.findByEmail(email).orElseThrow();
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("Can't find user by email " + email, e);
+        }
+
+        UserBuilder builder
+                = org.springframework.security.core.userdetails.User.withUsername(email);
+        builder.password(user.getPassword());
+        builder.authorities(user.getRoles().stream()
+                .map(r -> r.getName().name())
+                .toArray(String[]::new));
+        return builder.build();
+    }
+}
