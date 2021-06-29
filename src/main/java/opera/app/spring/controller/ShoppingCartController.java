6b31db3347963c8f07ce51215e9f3a9b7cf1545e
Maksim <max.freedman.main@gmail.com>
2021-06-29 16:44:59 +0300

    Changed shopping cart controller

--- src/main/java/opera/app/spring/controller/ShoppingCartController.java
+++ src/main/java/opera/app/spring/controller/ShoppingCartController.java
@@ -1,5 +1,6 @@
 package opera.app.spring.controller;
 
+import opera.app.spring.exception.DataProcessingException;
 import opera.app.spring.model.PerformanceSession;
 import opera.app.spring.model.User;
 import opera.app.spring.model.dto.response.ShoppingCartResponseDto;
@@ -7,11 +8,14 @@ import opera.app.spring.service.PerformanceSessionService;
 import opera.app.spring.service.ShoppingCartService;
 import opera.app.spring.service.UserService;
 import opera.app.spring.service.dto.mapping.impl.response.ShoppingCartResponseMapper;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Min;
 
 @RestController
 @RequestMapping("/shopping-carts")
@@ -31,18 +35,22 @@ public class ShoppingCartController {
         this.shoppingCartResponseMapper = shoppingCartResponseMapper;
     }
 
-    @PostMapping("/stage")
-    public ShoppingCartResponseDto addMovieSession(@RequestParam Long userId,
-                                                   @RequestParam Long stageId) {
-        User user = userService.get(userId);
-        PerformanceSession performanceSession = performanceSessionService.get(stageId);
-        shoppingCartService.addSession(performanceSession, user);
-        return shoppingCartResponseMapper.toDto(shoppingCartService.getByUser(user));
+    @PostMapping("/performance-sessions")
+    public ShoppingCartResponseDto addMovieSession(Authentication authentication,
+                                                   @RequestParam @Min(1) Long sessionId) {
+        String email = authentication.getName();
+        User userFromDb = userService.findByEmail(email)
+                .orElseThrow(() -> new DataProcessingException("User not found by email + " + email));
+        PerformanceSession performanceSession = performanceSessionService.get(sessionId);
+        shoppingCartService.addSession(performanceSession, userFromDb);
+        return shoppingCartResponseMapper.toDto(shoppingCartService.getByUser(userFromDb));
     }
 
     @GetMapping("/by-user")
-    public ShoppingCartResponseDto getByUser(@RequestParam Long userId) {
-        User user = userService.get(userId);
-        return shoppingCartResponseMapper.toDto(shoppingCartService.getByUser(user));
+    public ShoppingCartResponseDto getByUser(Authentication authentication) {
+        String email = authentication.getName();
+        User userFromDb = userService.findByEmail(email)
+                .orElseThrow(() -> new DataProcessingException("User not found by email + " + email));
+        return shoppingCartResponseMapper.toDto(shoppingCartService.getByUser(userFromDb));
     }
 }
